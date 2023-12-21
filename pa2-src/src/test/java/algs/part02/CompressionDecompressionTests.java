194be509532d52bc5484b88bd46e069d55de1051
George Beshay <georgebeshay101@gmail.com>
2023-12-21 08:22:15 +0200

    - Modified the gradle configurations to increase the heap size for tests.

--- pa2-src/src/test/java/algs/part02/CompressionDecompressionTests.java
+++ pa2-src/src/test/java/algs/part02/CompressionDecompressionTests.java
@@ -4,11 +4,9 @@ import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-
 import java.io.File;
 import java.io.IOException;
 import java.util.stream.Stream;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CompressionDecompressionTests {
@@ -38,34 +36,16 @@ public class CompressionDecompressionTests {
             throw new RuntimeException(e);
         }
     }
-
-//    private static boolean areFilesEqual(File file1, File file2) {
-//        try (BufferedInputStream bis1 = new BufferedInputStream(new FileInputStream(file1));
-//             BufferedInputStream bis2 = new BufferedInputStream(new FileInputStream(file2))) {
-//
-//            int byte1, byte2;
-//            while ((byte1 = bis1.read()) != -1) {
-//                byte2 = bis2.read();
-//                if (byte1 != byte2) {
-//                    return false;
-//                }
-//            }
-//            return bis2.read() == -1; // Ensure file2 has no extra content
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
-
     private static Stream<Arguments> generateAbsoluteFilePath() {
         return Stream.of(
                 Arguments.of(
                         "D:\\College\\Level 3\\Fall 2023 - 2024\\CSE 321 - Analysis and Design of Algorithms\\Programming Assignments\\Assignment 02\\algo-pa2\\pa2-src\\src\\test\\resources\\sample files\\Algorithms - Lectures 7 and 8 (Greedy algorithms).pdf",
                         1
                 ),
-//                Arguments.of(
-//                        "D:\\College\\Level 3\\Fall 2023 - 2024\\CSE 321 - Analysis and Design of Algorithms\\Programming Assignments\\Assignment 02\\algo-pa2\\pa2-src\\src\\test\\resources\\sample files\\recorded_video.mp4",
-//                        1
-//                ),
+                Arguments.of(
+                        "D:\\College\\Level 3\\Fall 2023 - 2024\\CSE 321 - Analysis and Design of Algorithms\\Programming Assignments\\Assignment 02\\algo-pa2\\pa2-src\\src\\test\\resources\\sample files\\recorded_video.mp4",
+                        1
+                ),
                 Arguments.of(
                         "D:\\College\\Level 3\\Fall 2023 - 2024\\CSE 321 - Analysis and Design of Algorithms\\Programming Assignments\\Assignment 02\\algo-pa2\\pa2-src\\src\\test\\resources\\sample files\\simpletext.txt",
                         1
